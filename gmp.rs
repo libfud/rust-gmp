4ad34e21be955be9f5351f0f1320bb5e01f25b9b
libfud <libfud@librehumanitas.org>
2014-07-01 08:51:11 -0400

    removed all tildas

--- gmp.rs
+++ gmp.rs
@@ -340,7 +340,7 @@ impl Div<Mpz, Mpz> for Mpz {
     fn div(&self, other: &Mpz) -> Mpz {
         unsafe {
             if self.is_zero() {
-                fail!(~"divide by zero")
+                fail!("divide by zero")
             }
 
             let mut res = Mpz::new();
@@ -354,7 +354,7 @@ impl Rem<Mpz, Mpz> for Mpz {
     fn rem(&self, other: &Mpz) -> Mpz {
         unsafe {
             if self.is_zero() {
-                fail!(~"divide by zero")
+                fail!("divide by zero")
             }
 
             let mut res = Mpz::new();
@@ -376,10 +376,10 @@ impl Neg<Mpz> for Mpz {
 
 impl ToPrimitive for Mpz {
     fn to_i64(&self) -> Option<i64> {
-        fail!(~"not implemented")
+        fail!("not implemented")
     }
     fn to_u64(&self) -> Option<u64> {
-        fail!(~"not implemented")
+        fail!("not implemented")
     }
 }
 
@@ -480,13 +480,13 @@ impl FromStr for Mpz {
 
 impl ToStrRadix for Mpz {
     // TODO: fail on an invalid base
-    fn to_str_radix(&self, base: uint) -> ~str {
+    fn to_str_radix(&self, base: uint) -> str {
         unsafe {
             // Extra two bytes are for possible minus sign and null terminator
             let len = __gmpz_sizeinbase(&self.mpz, base as c_int) as uint + 2;
 
             // Allocate and write into a raw *c_char of the correct length
-            let mut vector: ~[u8] = slice::with_capacity(len);
+            let mut vector: [u8] = slice::with_capacity(len);
             vector.set_len(len);
 
             let mut cstr = vector.to_c_str_unchecked();
@@ -649,7 +649,7 @@ impl Mpq {
     pub fn invert(&self) -> Mpq {
         unsafe {
             if self.is_zero() {
-                fail!(~"divide by zero")
+                fail!("divide by zero")
             }
 
             let mut res = Mpq::new();
@@ -725,7 +725,7 @@ impl Div<Mpq, Mpq> for Mpq {
     fn div(&self, other: &Mpq) -> Mpq {
         unsafe {
             if self.is_zero() {
-                fail!(~"divide by zero")
+                fail!("divide by zero")
             }
 
             let mut res = Mpq::new();
@@ -747,10 +747,10 @@ impl Neg<Mpq> for Mpq {
 
 impl ToPrimitive for Mpq {
     fn to_i64(&self) -> Option<i64> {
-        fail!(~"not implemented")
+        fail!("not implemented")
     }
     fn to_u64(&self) -> Option<u64> {
-        fail!(~"not implemented")
+        fail!("not implemented")
     }
 }
 
@@ -924,7 +924,7 @@ impl Div<Mpf, Mpf> for Mpf {
     fn div(&self, other: &Mpf) -> Mpf {
         unsafe {
             if __gmpf_cmp_ui(&self.mpf, 0) == 0 {
-                fail!(~"divide by zero")
+                fail!("divide by zero")
             }
 
             let mut res = Mpf::new(cmp::max(self.get_prec() as uint,
@@ -1054,13 +1054,13 @@ mod test_mpz {
     #[test]
     fn test_to_str_radix() {
         let x: Mpz = FromPrimitive::from_int(255).unwrap();
-        assert!(x.to_str_radix(16) == ~"ff");
+        assert!(x.to_str_radix(16) == "ff");
     }
 
     #[test]
     fn test_to_str() {
         let x: Mpz = FromStr::from_str("1234567890").unwrap();
-        assert!(x.to_str() == ~"1234567890");
+        assert!(x.to_str() == "1234567890");
     }
 
     #[test]
@@ -1081,7 +1081,7 @@ mod test_mpz {
     #[test]
     fn test_from_int() {
         let x: Mpz = FromPrimitive::from_int(150).unwrap();
-        assert!(x.to_str() == ~"150");
+        assert!(x.to_str() == "150");
         assert!(x == FromStr::from_str("150").unwrap());
     }
 
